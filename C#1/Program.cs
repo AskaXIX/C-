03be9663ddecd9a4bc41bebb6974bc7ec155afee
Aslan <aslan.aslan.14@bk.ru>
2023-04-24 03:02:07 +0600

    added some changes to c#1

--- C#1/Program.cs
+++ C#1/Program.cs
@@ -5,43 +5,79 @@
 Console.WriteLine("Insert number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number == 1)
-{
-    Console.WriteLine("Its Monday");
-}
+// if (number == 1)
+// {
+//     Console.WriteLine("Its Monday");
+// }
+// else if (number == 2)
+// {
+//     Console.WriteLine("Its Tuesday");
+// }
+// else if (number == 3)
+// {
+//     Console.WriteLine("Its Wednesday");
+// }
+// else if (number == 4)
+// {
+//     Console.WriteLine("Its Thirsday");
+// }
+// else if (number == 5)
+// {
+//     Console.WriteLine("Its Friday");
+// }
+// else if (number == 6)
+// {
+//     Console.WriteLine("Its Saturday");
+// }
+// else if (number == 7)
+// {
+//     Console.WriteLine("Its Sunday");
+// }
+// else
+// {
+//     Console.WriteLine("Error!");
+// }
 
-else if (number == 2)
+switch (number)
 {
-    Console.WriteLine("Its Tuesday");
+    case 1:
+        {
+            Console.WriteLine("Its Monday");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Its Tuesday");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("Its Wednesday");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("Its Thirsday");
+            break;
+        }
+    case 5:
+        {
+            Console.WriteLine("Its Friday");
+            break;
+        }
+    case 6:
+        {
+            Console.WriteLine("Its Saturday");
+            break;
+        }
+    case 7:
+        {
+            Console.WriteLine("Its Sunday");
+            break;
+        }
+    default:
+        {
+            System.Console.WriteLine("Error!");
+            break;
+        }
 }
-
-else if (number == 3)
-{
-    Console.WriteLine("Its Wednesday");
-}
-
-else if (number == 4)
-{
-    Console.WriteLine("Its Thirsday");
-}
-
-else if (number == 5)
-{
-    Console.WriteLine("Its Friday");
-}
-
-else if (number == 6)
-{
-    Console.WriteLine("Its Saturday");
-}
-
-else if (number == 7)
-{
-    Console.WriteLine("Its Sunday");
-}
-
-else
-{
-    Console.WriteLine("Error!");
-}
-
