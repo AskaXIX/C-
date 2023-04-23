bda1fd71c2155acc63feacadc35313d27db16c4e
Aslan <aslan.aslan.14@bk.ru>
2023-04-23 22:49:36 +0600

    some new changes in C#1 and C#

--- C#1/Program.cs
+++ C#1/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine("Insert number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Its Monday");
+}
+
+else if (number == 2)
+{
+    Console.WriteLine("Its Tuesday");
+}
+
+else if (number == 3)
+{
+    Console.WriteLine("Its Wednesday");
+}
+
+else if (number == 4)
+{
+    Console.WriteLine("Its Thirsday");
+}
+
+else if (number == 5)
+{
+    Console.WriteLine("Its Friday");
+}
+
+else if (number == 6)
+{
+    Console.WriteLine("Its Saturday");
+}
+
+else if (number == 7)
+{
+    Console.WriteLine("Its Sunday");
+}
+
+else
+{
+    Console.WriteLine("Error!");
+}
+
